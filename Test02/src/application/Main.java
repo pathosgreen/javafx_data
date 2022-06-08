98a6ff5dac681bbb6b9c2a3473fb1888ed5e3c87
DESKTOP-P2E4GPS\505 <hexun_@naver.com>
2022-06-08 15:48:54 +0900

    javafx_data

---
+++ Test02/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			// stage > scene > container > node
+			
+			Pane pane = new Pane(); // container
+			
+			Scene scene = new Scene(pane,300,500);
+			
+			stage.setScene(scene);
+			stage.show();
+			stage.setTitle("My Windows");
+			stage.setResizable(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
